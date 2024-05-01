4b196caf0c9f78f0af2226a74511910414ef5da6
Amr Saber <amr.m.saber.mail@gmail.com>
2024-05-01 18:49:20 +0100

    refactor and allow early cancelling

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
-module go-timer
+module timer
 
-go 1.21.0
+go 1.22.2
+
+require github.com/gosuri/uilive v0.0.4
 
 require (
-	github.com/gosuri/uilive v0.0.4 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"math"
 	"os"
+	"os/signal"
+	"regexp"
+	"syscall"
 	"time"
 
 	"github.com/gosuri/uilive"
 )
+
+var numberRegex = regexp.MustCompile(`^-?\d+$`)
 
 func main() {
 	if len(os.Args) == 1 {
@@ -15,9 +22,32 @@ func main() {
 		os.Exit(1)
 	}
 
+	waitDuration, err := parseDuration()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error parsing duration(s):\n%s\n", err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	handleSigterm(ctx, cancel)
+	countDown(ctx, waitDuration)
+
+	if ctx.Err() != nil {
+		os.Exit(1)
+	}
+}
+
+func parseDuration() (time.Duration, error) {
 	var waitDuration time.Duration
 	parsingErrs := make([]error, 0)
 	for _, amount := range os.Args[1:] {
+		if numberRegex.MatchString(amount) {
+			amount = amount + "s"
+		}
+
 		duration, err := time.ParseDuration(amount)
 		if err != nil {
 			parsingErrs = append(parsingErrs, fmt.Errorf("invalid duration %q", amount))
@@ -29,29 +59,54 @@ func main() {
 
 	if len(parsingErrs) > 0 {
 		parsingErr := errors.Join(parsingErrs...)
-		fmt.Fprintf(os.Stderr, "error parsing duration(s):\n%s\n", parsingErr)
-		os.Exit(1)
+		return 0, parsingErr
 	}
 
 	waitDuration = waitDuration.Round(time.Second)
 
+	return waitDuration, nil
+}
+
+func handleSigterm(ctx context.Context, fn func()) {
+	go func() {
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
+
+		select {
+		case <-signals:
+			fn()
+		case <-ctx.Done():
+		}
+	}()
+}
+
+func countDown(ctx context.Context, duration time.Duration) {
 	uiWriter := uilive.New()
 	uiWriter.Start()
 
 	now := time.Now()
-	target := now.Add(waitDuration)
+	target := now.Add(duration)
+
+loop:
 	for now.Before(target) {
 		remaining := target.Sub(now)
 
-		fmt.Fprintf(
-			uiWriter,
-			"%02d:%02d:%02d\n",
-			int(remaining.Hours()),
-			int(remaining.Minutes())%60,
-			(int(remaining.Seconds())-int(remaining.Minutes())*60)%60,
-		)
+		seconds := int(math.Ceil(remaining.Seconds()))
+		minutes := seconds / 60
+		hours := minutes / 60
 
-		time.Sleep(253 * time.Millisecond)
+		seconds %= 60
+		minutes %= 60
+
+		fmt.Fprintf(uiWriter, "%02d:%02d:%02d\n", hours, minutes, seconds)
+
+		// Sleep or exit
+		select {
+		case <-time.After(253 * time.Millisecond):
+		case <-ctx.Done():
+			break loop
+		}
+
 		now = time.Now()
 	}
 
